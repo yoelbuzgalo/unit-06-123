2047c20f131bc4d23f192237899b0842f25a9661
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-10-06 10:56:19 -0400

    Activity 6.3 and 6.4

--- activities.py
+++ activities.py
@@ -6,8 +6,22 @@ def making_arrays():
     print(arrays.Array(10, ""))
     print(arrays.Array(20, False))
 
+def while_fill(an_array):
+    index = 0
+    while index < len(an_array):
+        an_array[index] = index
+        index += 1
+    return an_array
+
+def for_fill(an_array):
+    for index in range(len(an_array)):
+        an_array[index] = index
+    return an_array
+
 def main():
-    making_arrays()
+    # making_arrays()
+    # print(while_fill(arrays.Array(10)))
+    print(for_fill(arrays.Array(10)))
 
 if __name__ == "__main__":
     main()
