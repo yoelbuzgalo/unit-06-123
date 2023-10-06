5bd906fb5cb434bfa129a8ef83c04a4984a1d65c
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-10-06 11:24:57 -0400

    Activity 6.8

--- array_untils.py
+++ array_untils.py
@@ -9,10 +9,18 @@ def random_array(size, min_value=0, max_value=None):
         new_arr[index] = random.randint(min_value, max_value)
     return new_arr
 
+def range_array(start, stop, step=1):
+    a_range = range(start, stop, step)
+    an_array = arrays.Array(len(a_range))
+    for index in range(len(an_array)):
+        an_array[index] = a_range[index]
+    return an_array
+
 def main():
-    random.seed(1)
-    an_array = random_array(10)
-    print(an_array)
+    # random.seed(1)
+    # an_array = random_array(10)
+    # print(an_array)
+    print(range_array(1, 10))
 
 if __name__ == "__main__":
     main()
