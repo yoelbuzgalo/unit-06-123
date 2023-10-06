ec22766719a0d5a2de542cba5608961d1440d6d2
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-10-06 10:20:40 -0400

    Unit 05 Mini Practicum (Added comments and fixed some)

--- mini_practicum/practice.py
+++ mini_practicum/practice.py
@@ -2,27 +2,21 @@ import csv
 import re
 
 def find_rabbit():
-    count = 0
-    with open("alice.txt") as file:
-        header = next(file).strip().split()
-        for line in file:
-            line = line.strip().split()
-            for word in line:
+    count = 0 # Keeps track of how many 'Rabbit' word was found
+    with open("alice.txt") as file: # Opens the file
+        for line in file: # Iterates over every line in file
+            line = line.strip().split() # Remove whitespaces and splits words
+            for word in line: # Iterates words in a splitted line
                 if word == "Rabbit":
-                    count += 1
-    ####Your code goes here
-
+                    count += 1 # Adds to the count tracker for every match of word 'Rabbit'
     return count
 
 def find_rabbit_regex ():
-    count = 0
-    with open("alice.txt") as file:
-        for line in file:
-            if re.findall("[A-Za-z]*Rabbit", line) or re.findall("Rabbit[A-Za-z]*", line):
-                count += 1
-    
-    ####Your code goes here
-
+    count = 0 # Keeps track of how many 'Rabbit' word was found
+    with open("alice.txt") as file: # Opens the file
+        for line in file: # Iterates over every line in file
+            if re.findall("[A-Za-z]*Rabbit", line) or re.findall("Rabbit[A-Za-z]*", line): # Uses regular expression to find all matched words in a line based on pattern
+                count += 1 # Adds to the count tracker for every match of word 'Rabbit'
     return count
 
 def main ():
