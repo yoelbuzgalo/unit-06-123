833e035770a829afbdd7b2ece742b1cbf377f382
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-10-06 10:47:43 -0400

    Activity 6.2

---
+++ activities.py
@@ -0,0 +1,13 @@
+import arrays
+
+def making_arrays():
+    print(arrays.Array(5))
+    print(arrays.Array(1, 0))
+    print(arrays.Array(10, ""))
+    print(arrays.Array(20, False))
+
+def main():
+    making_arrays()
+
+if __name__ == "__main__":
+    main()
